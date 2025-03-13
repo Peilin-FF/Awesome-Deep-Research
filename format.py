15a5641e90e6bd71e45467b4903aac761a4b0678
Peilin-FF <1092854742@qq.com>
2025-03-13 22:07:20 +0800

    update

--- format.py
+++ format.py
@@ -45,7 +45,7 @@ def main():
         url = sys.argv[1]
     else:
         # 从用户输入获取URL
-        url = "https://github.com/THUKElab/LatEval"
+        url = "https://github.com/gkamradt/LLMTest_NeedleInAHaystack"
     
     formatted_output = format_github_url(url)
     print(formatted_output)
